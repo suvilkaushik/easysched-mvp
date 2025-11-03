0560b1112ed38f7afe885e196b40592a4e4c43fb
SUVIL KAUSHIK <suvilk@udel.edu>
2025-11-03 18:41:09 -0500

    moved client portal button to footer

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -43,14 +43,6 @@ export default function Navigation() {
               })}
             </div>
           </div>
-          <div className="flex items-center">
-            <Link
-              href="/client-portal"
-              className="ml-4 px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700"
-            >
-              Client Portal
-            </Link>
-          </div>
         </div>
       </div>
     </nav>
